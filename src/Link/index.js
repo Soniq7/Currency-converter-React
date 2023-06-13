84021791e9bc3a71bbe8aca3f1901a17d63e8aaf
Sonia Skowron-Zarebska <ssoniq7@gmail.com>
2023-06-13 19:30:20 +0100

    Link description changed in index.js

--- src/Link/index.js
+++ src/Link/index.js
@@ -4,8 +4,7 @@ const Link = () => {
   return (
     <div className="link">
       <p>
-        For more currency exchange information please check website:
-        <br />
+        British Pound exchange rates comes from <br />x-rates.com table dated 29-03-2023:<br />
         <a
           className="link__website"
           href="https://www.x-rates.com/table/?from=GBP&amount=1"
