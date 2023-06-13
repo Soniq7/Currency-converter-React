d8b073128ccb21d8687e89a1f82a1f498f7a18a3
Sonia Skowron-Zarebska <ssoniq7@gmail.com>
2023-06-13 13:14:38 +0100

    initial App.js body updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <body class="body">
+    <form class="form js-form">
+      <fieldset class="form__fieldset">
+        <legend class="form__legend">Currency converter</legend>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <label>
+            <span class="form__label"> Amount in GBP * : </span>
+            <input
+              placeholder="Enter amount in GBP"
+              class="form__field js-amount"
+              type="number"
+              name="amount"
+              required
+              step="0.01"
+              min="0.01"
+            />
+          </label>
         </p>
+        <p>
+          <label>
+            <span class="form__label"> Currency : </span>
+            <select class="form__field js-currency">
+              <option value="EUR" selected>EUR Euro</option>
+              <option value="PLN">PLN Polish Zloty</option>
+              <option value="USD">USD US Dollar</option>
+            </select>
+          </label>
+        </p>
+        <p class="form__field form__field--mandatory">
+          * Mandatory fields<br />
+          <span class="form__field form__field--pattern">
+            (for float numbers please use dot ".")
+          </span>
+        </p>
+      </fieldset>
+      <p>
+        <button class="form__button">Convert</button>
+      </p>
+      <p class="form__result js-result">Result :</p>
+    </form>
+    <div class="link">
+      <p>
+        For more currency exchange information please check website:<br />
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          class="link__website"
+          href="https://www.x-rates.com/table/?from=GBP&amount=1"
+          >"BRITISH POUND EXCHANGE RATES TABLE"
         </a>
-      </header>
+      </p>
     </div>
+  </body>
   );
 }
 
