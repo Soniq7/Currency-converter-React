c0469d58a5d3b1ccdb785fdb35bb198421e9c11f
Sonia Skowron-Zarebska <ssoniq7@gmail.com>
2023-06-19 02:24:31 +0100

    interactions added to Form

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,14 +1,38 @@
+import React, { useState } from "react";
 import "./style.css";
+import currencies from "./currencies";
 
-const Form = () => {
-    return (
-    <form className="form">
+function Form() {
+  const [amount, setAmount] = useState("");
+  const [currency, setCurrency] = useState("EUR");
+  const [result, setResult] = useState(null);
+
+  const calculateResult = () => {
+    const selectedCurrency = currencies.find(
+      (currency) => currency.id === selectedCurrency
+    );
+    if (selectedCurrency) {
+      return amount * selectedCurrency.rate;
+    }
+    return 0;
+  };
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    const result = calculateResult();
+    setResult(result);
+  };
+
+  return (
+    <form className="form" onSubmit={onFormSubmit}>
       <fieldset className="form__fieldset">
         <legend className="form__legend">Currency converter</legend>
         <p>
           <label>
-            <span className="form__label"> Amount in GBP * : </span>
+            <span className="form__label"> Amount in GBP * :</span>
             <input
+              value={amount}
+              onChange={({ target }) => setAmount(target.value)}
               placeholder="Enter amount in GBP"
               className="form__field"
               type="number"
@@ -22,15 +46,22 @@ const Form = () => {
         <p>
           <label>
             <span className="form__label"> Currency : </span>
-            <select className="form__field">
-              <option value="EUR" selected>EUR Euro</option>
-              <option value="PLN">PLN Polish Zloty</option>
-              <option value="USD">USD US Dollar</option>
+            <select
+              className="form__field"
+              value={currency}
+              onChange={({ target }) => setCurrency(target.value)}
+            >
+              {currencies.map((currency) => (
+                <option key={currency.id} value={currency.id}>
+                  {currency.code} {currency.name}
+                </option>
+              ))}
             </select>
           </label>
         </p>
         <p className="form__field form__field--mandatory">
-          * Mandatory fields<br />
+          * Mandatory fields
+          <br />
           <span className="form__field form__field--pattern">
             (for float numbers please use dot ".")
           </span>
@@ -39,9 +70,18 @@ const Form = () => {
       <p>
         <button className="form__button">Convert</button>
       </p>
-      <p className="form__result">Result :</p>
+      <p className="form__result">
+        {result !== null && (
+          <>
+            Result: {amount} GBP =
+            <strong>
+              {result.toFixed(2)} {currency.code}
+            </strong>
+          </>
+        )}
+      </p>
     </form>
-    );
-};
+  );
+}
 
 export default Form;
