5b0ab3d876445465dffdb5a18fc0eb0a3de4cd4f
Sonia Skowron-Zarebska <ssoniq7@gmail.com>
2023-06-20 09:38:57 +0100

    <div> removed from Link component

--- src/Link/index.js
+++ src/Link/index.js
@@ -2,17 +2,17 @@ import "./style.css";
 
 const Link = () => {
   return (
-    <div className="link">
-      <p>
-        British Pound exchange rates come from <br />x-rates.com table dated 29-03-2023:<br />
-        <a
-          className="link__website"
-          href="https://www.x-rates.com/table/?from=GBP&amount=1"
-        >
-          "BRITISH POUND EXCHANGE RATES TABLE"
-        </a>
-      </p>
-    </div>
+    <p className="link">
+      British Pound exchange rates come from <br />
+      x-rates.com table dated 29-03-2023:
+      <br />
+      <a
+        className="link__website"
+        href="https://www.x-rates.com/table/?from=GBP&amount=1"
+      >
+        "BRITISH POUND EXCHANGE RATES TABLE"
+      </a>
+    </p>
   );
 };
 
