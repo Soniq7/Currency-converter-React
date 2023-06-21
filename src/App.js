9fe22339d876ea8b3306f3a96394999953a614dc
Sonia Skowron-Zarebska <ssoniq7@gmail.com>
2023-06-21 02:09:53 +0100

    Component Link removed from App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import Container from "./Container";
 import Form from "./Form";
-import Link from "./Link";
 
 function App() {
   return (
     <Container>
       <Form />
-      <Link />
     </Container>
   );
 }
