998ce95134499761ed4ff0702b366fe188e30ce1
Sonia Skowron-Zarebska <ssoniq7@gmail.com>
2023-06-28 20:46:00 +0100

    div added to Result component

--- src/App/Form/Result/index.js
+++ src/App/Form/Result/index.js
@@ -1,27 +1,27 @@
 import "./style.css";
 
 export const Result = ({ result }) => (
-  <p className="result">
-    {result !== null && (
-      <>
-        {result.sourceAmount.toFixed(2)}&nbsp;GBP&nbsp;=
-        {" "}
-        <strong>
-          {result.targetAmount.toFixed(2)}&nbsp;{result.currency}
-        </strong>
-      </>
-    )}
+  <div className="result">
+    <p>
+      {result !== null && (
+        <>
+          {result.sourceAmount.toFixed(2)}&nbsp;GBP&nbsp;={" "}
+          <strong>
+            {result.targetAmount.toFixed(2)}&nbsp;{result.currency}
+          </strong>
+        </>
+      )}
+    </p>
     <span className="result__link">
       <br />
-      British Pound exchange rates come from {" "}
+      British Pound exchange rates come from{" "}
       <a
         className="result__link--website"
         href="https://www.x-rates.com/table/?from=GBP&amount=1"
       >
         x-rates.com
-      </a> 
-      {" "} table dated 29-03-2023
+      </a>{" "}
+      table dated 29-03-2023
     </span>
-  </p>
+  </div>
 );
-
