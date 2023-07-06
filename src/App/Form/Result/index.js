97fa7c952efa41fe487e5f40b6dda1e11296f159
Soniq7 <ssoniq7@gmail.com>
2023-07-06 16:08:23 +0100

    Result link moved above result amount

--- src/App/Form/Result/index.js
+++ src/App/Form/Result/index.js
@@ -2,6 +2,16 @@ import "./style.css";
 
 export const Result = ({ result }) => (
   <div className="result">
+    <span className="result__link">
+      British Pound exchange rates come from{" "}
+      <a
+        className="result__link--website"
+        href="https://www.x-rates.com/table/?from=GBP&amount=1"
+      >
+        x-rates.com
+      </a>{" "}
+      table dated 29-03-2023
+    </span>
     <p className="result__amount">
       {result !== null && (
         <>
@@ -12,16 +22,5 @@ export const Result = ({ result }) => (
         </>
       )}
     </p>
-    <span className="result__link">
-      <br />
-      British Pound exchange rates come from{" "}
-      <a
-        className="result__link--website"
-        href="https://www.x-rates.com/table/?from=GBP&amount=1"
-      >
-        x-rates.com
-      </a>{" "}
-      table dated 29-03-2023
-    </span>
   </div>
 );
