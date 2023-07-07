a7a985e00bd08d7aa1f8ff22b8e5c148516c89dc
Soniq7 <ssoniq7@gmail.com>
2023-07-07 15:21:22 +0100

    theme.js created and imported to App index.js

---
+++ src/theme.js
@@ -0,0 +1,18 @@
+
+export const theme = {
+    color: {
+        darkNavy: "#010033",
+        navy: "#010047",
+        lightNavy: "#0f0178",
+        greyBlue: "#a3c3d8",
+        skyBlue: "#197ca3",
+        blue: "#0f01c8",
+        lightBlue: "#0f20fa",
+        lighterBlue: "#0055b6;",
+        white: "#ffffff",
+        lightBlack: "#222",
+    },
+    breakpoint: {
+        mobileMax: 767,
+    },
+};
