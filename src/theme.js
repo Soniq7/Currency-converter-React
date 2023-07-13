c4ed601c325f312826f524b6965784f337d27ab7
Soniq7 <ssoniq7@gmail.com>
2023-07-13 18:57:24 +0100

    theme color names changed

--- src/theme.js
+++ src/theme.js
@@ -1,16 +1,13 @@
 
 export const theme = {
     color: {
-        darkNavy: "#010033",
-        navy: "#010047",
-        lightNavy: "#0f0178",
-        greyBlue: "#a3c3d8",
-        skyBlue: "#197ca3",
-        blue: "#0f01c8",
-        lightBlue: "#0f20fa",
-        lighterBlue: "#0055b6;",
+        stratos: "#010033",
+        navyBlue: "#000080",
+        pigeonPost: "#a3c3d8",
+        matisse: "#197ca3",
+        endeavour: "#0055b6;",
         white: "#ffffff",
-        lightBlack: "#222",
+        mineShaft: "#222",
     },
     breakpoint: {
         mobileMax: 767,
