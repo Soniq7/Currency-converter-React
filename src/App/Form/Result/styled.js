d32d20eebca2d0a8c91fa1df22dea6c39ef5cd5d
Soniq7 <ssoniq7@gmail.com>
2023-07-15 04:10:18 +0100

    Wrapper styled moved to Form, styled Link removed

--- src/App/Form/Result/styled.js
+++ src/App/Form/Result/styled.js
@@ -4,29 +4,6 @@ export const StyledResult = styled.div`
   text-align: center;
 `;
 
-export const Wrapper = styled.span`
-  font-size: small;
-  color: ${({ theme }) => theme.color.stratos};
-`;
-
-export const Link = styled.a`
-  color: ${({ theme }) => theme.color.navyBlue};
-  text-decoration: none;
-  font-weight: bold;
-
-  &:visited {
-    filter: brightness(90%);
-  }
-
-  &:hover {
-    filter: brightness(150%);
-  }
-
-  &:active {
-    filter: brightness(200%);
-  }
-`;
-
 export const Amount = styled.p`
-  margin: 30px;
+  margin: 30px 30px 40px 30px;
 `;
