57568abd7491a6ec3b770c8c4a9121ba5c2f5716
Soniq7 <ssoniq7@gmail.com>
2023-07-15 04:07:49 +0100

    Wrapper moved from Result component to Form

--- src/App/Form/Result/index.js
+++ src/App/Form/Result/index.js
@@ -1,17 +1,7 @@
-import { StyledResult, Wrapper, Link, Amount } from "./styled";
+import { StyledResult, Amount } from "./styled";
 
 export const Result = ({ result }) => (
   <StyledResult>
-    <Wrapper>
-      British Pound exchange rates come from{" "}
-      <Link as="a"
-        className="result__link--website"
-        href="https://www.x-rates.com/table/?from=GBP&amount=1"
-      >
-        x-rates.com
-      </Link>{" "}
-      table dated 29-03-2023
-    </Wrapper>
     <Amount>
       {result !== null && (
         <>
