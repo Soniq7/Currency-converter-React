a547c7f9f87b9a6b72759ee3406efe5ff02783ec
Soniq7 <ssoniq7@gmail.com>
2023-07-15 04:05:41 +0100

    result / calculateResult moved from App to Form

--- src/App/index.js
+++ src/App/index.js
@@ -1,27 +1,11 @@
-import { useState } from "react";
 import { Container } from "./styled";
 import { Form } from "./Form";
-import { currencies } from "./currencies";
 
 function App() {
-  const [result, setResult] = useState(null);
-
-  const calculateResult = (currency, amount) => {
-    const rate = currencies.find(({ code }) => code === currency).rate;
-
-    setResult({
-      sourceAmount: +amount,
-      targetAmount: amount * rate,
-      currency,
-    });
-  };
 
   return (
     <Container>
-      <Form 
-        result={result} 
-        calculateResult={calculateResult} 
-      />
+      <Form />
     </Container>
   );
 }
